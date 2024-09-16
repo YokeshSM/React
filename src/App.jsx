cd9f674510384aeb2af60e19fe4a33ac67071c35
YokeshSM <yokesh2003783@gmail.com>
2024-09-16 12:14:25 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-     <div className="h-screen w-screen flex justify-center items-center bg-sky-500">
-     <div className="h-3/6 w-3/6 flex justify-center items-center bg-white rounded-md shadow-2x1 ">
-     <form action="" className="flex flex-col justify-center items-center gap-4 rounded-md">
-      <input className=" bg-slate-200 "></input>
-      <input className=" bg-slate-200 " ></input>
-      <input className=" bg-slate-200 "></input>
-      <input className=" bg-slate-200 "></input>
-     </form>
-     </div>
-     </div>
-    </>
+    <div className="bg-purple-500">
+      Hello Hi
+    </div>
   )
 }
 
